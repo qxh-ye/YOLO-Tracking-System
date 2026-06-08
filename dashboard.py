a72152321acd699caa8ab3763e281f9636548aea
qxh <qxh@123626>
2026-06-08 22:00:02 +0800

    feat: add flask dashboard v1

---
+++ dashboard.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+data = {
+    "fps": 15.6,
+    "roi_count": 3,
+    "enter_count": 5,
+    "exit_count": 2,
+    "events": [
+        "ID 1 ROI ENTER",
+        "ID 3 LINE ENTER"
+    ]
+}
+
+@app.route("/")
+def index():
+    return render_template(
+        "index.html",
+        data=data
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
